9ecef85d9e4516b77a8e517c698a70a1eb618990
portegys <portegys@gmail.com>
2022-10-28 10:59:19 -0500

    Version 2.0

--- src/main/java/morphognosis/nestingbirds/NestingBirds.java
+++ src/main/java/morphognosis/nestingbirds/NestingBirds.java
@@ -11,7 +11,7 @@ import morphognosis.nestingbirds.Bird.RESPONSE;
 public class NestingBirds
 {
    // Version.
-   public static final String VERSION = "1.0";
+   public static final String VERSION = "2.0";
 
    // Random numbers.
    public static int RANDOM_NUMBER_SEED = 4517;
